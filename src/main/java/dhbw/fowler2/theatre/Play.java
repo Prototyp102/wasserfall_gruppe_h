86cff1093575fd53f363afa5221ca2798888d886
Simon Kritzer <simon.kritzer@hotmail.de>
2021-03-13 13:30:10 +0100

    Anforderung 105.1 - Erweiterung von Theatergenres in der Result-Klasse

--- src/main/java/dhbw/fowler2/theatre/Play.java
+++ src/main/java/dhbw/fowler2/theatre/Play.java
@@ -16,10 +16,31 @@ public class Play {
         int result = 0;
         switch(play.type){
             case "tragedy":
-                result = new TragedyResult().getResult(perf);
+                result = new TragicalResult().getResult(perf);
                 break;
             case "comedy":
-                result = new ComedyResult().getResult(perf);
+                result = new ComedicResult().getResult(perf);
+                break;
+            case "history":
+                result = new HistoricalResult().getResult(perf);
+                break;
+            case "pastoral-comical":
+                result = new PastoralComicalResult().getResult(perf);
+                break;
+            case "historical-pastoral":
+                result = new HistoricalPastoralResult().getResult(perf);
+                break;
+            case "tragicalhistorical":
+                result = new TragicalHistoricalResult().getResult(perf);
+                break;
+            case "tragical-comical-historical-pastoral":
+                result = new TragicalComicalHistoricalPastoralResult().getResult(perf);
+                break;
+            case "scene-individable":
+                result = new SceneIndividableResult().getResult(perf);
+                break;
+            case "poem-unlimited":
+                result = new PoemUnlimitedResult().getResult(perf);
                 break;
             default:
                 throw new Error("unknown type: " + play.type);
--- src/main/java/dhbw/fowler2/theatre/Result.java
+++ src/main/java/dhbw/fowler2/theatre/Result.java
@@ -5,7 +5,7 @@ public abstract class Result {
     public abstract int getResult(Performance perf);
 
 }
-class TragedyResult extends Result {
+class TragicalResult extends Result {
     public int getResult(Performance perf) {
         result = 40000;
         if (perf.audience > 30) {
@@ -14,13 +14,89 @@ class TragedyResult extends Result {
         return result;
     }
 }
-class ComedyResult extends Result {
+class ComedicResult extends Result {
     public int getResult(Performance perf) {
         result = 30000;
         if (perf.audience > 20) {
             result += 10000 + 500 * (perf.audience - 20);
         }
         result += 300* perf.audience;
+        return result;
+    }
+}
+
+class HistoricalResult extends Result {
+    public int getResult(Performance perf) {
+        result = 25000;
+        if (perf.audience > 40) {
+            result += 7482 + 500 * (perf.audience - 20);
+        }
+        return result;
+    }
+}
+class PastoralResult extends Result {
+    public int getResult(Performance perf) {
+        result = 2500;
+        if (perf.audience > 100) {
+            result += 13500 + 400 * (perf.audience - 20);
+        }
+        result += 150 * perf.audience;
+        return result;
+    }
+}
+class PastoralComicalResult extends Result {
+    public int getResult(Performance perf) {
+        result = 8000;
+        if (perf.audience > 20) {
+            result += 15000 + 120 * (perf.audience - 20);
+        }
+        return result;
+    }
+}
+class HistoricalPastoralResult extends Result {
+    public int getResult(Performance perf) {
+        result = 9500;
+        if (perf.audience > 10) {
+            result += 750 + 169 * (perf.audience - 20);
+        }
+        return result;
+    }
+}
+class TragicalHistoricalResult extends Result {
+    public int getResult(Performance perf) {
+        result = 13500;
+        if (perf.audience > 50) {
+            result += 2380 + 200 * (perf.audience - 20);
+        }
+        return result;
+    }
+}
+class TragicalComicalHistoricalPastoralResult extends Result {
+    public int getResult(Performance perf) {
+        result = 50000;
+        if (perf.audience > 20) {
+            result += 12500 + 400 * (perf.audience - 20);
+        }
+        result += 250* perf.audience;
+        return result;
+    }
+}
+class SceneIndividableResult extends Result {
+    public int getResult(Performance perf) {
+        result = 6900;
+        if (perf.audience > 20) {
+            result += 1337 + 420 * (perf.audience - 20);
+        }
+        result += 69 * perf.audience;
+        return result;
+    }
+}
+class PoemUnlimitedResult extends Result {
+    public int getResult(Performance perf) {
+        result = 35000;
+        if (perf.audience > 5) {
+            result += 7200 + 291 * (perf.audience - 20);
+        }
         return result;
     }
 }
