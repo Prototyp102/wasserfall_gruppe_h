83faa4a7c786fa708bf68d3cac21c0d9ddd425dc
Simon Kritzer <simon.kritzer@hotmail.de>
2021-03-13 11:39:47 +0100

    Berechnungsmethode extrahiert, in Play ausgelagert und an alter Stelle aufgerufen

--- src/main/java/dhbw/fowler2/theatre/Play.java
+++ src/main/java/dhbw/fowler2/theatre/Play.java
@@ -9,4 +9,27 @@ public class Play {
         this.name = name;
         this.type = type;
     }
+
+    public double amountFor(Performance aPerformance, Play play) {
+        int result = 0;
+        switch(play.type){
+            case "tragedy":
+                result = 40000;
+                if(aPerformance.audience > 30) {
+                    result += 1000*(aPerformance.audience-30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if(aPerformance.audience > 20) {
+                    result += 10000 + 500*(aPerformance.audience-20);
+                }
+                result+= 300*aPerformance.audience;
+                break;
+            default:
+                throw new Error("unknown type: " + play.type);
+
+        }
+        return result;
+    }
 }
