54e41b544c46f7efda79ff71f91c07717c391bca
Raphael Alexander Rahn <raphaelrahn1@gmail.com>
2021-03-13 12:04:06 +0100

    Anforderung 102.2 umgesetzt: Temp.-Variablen Play und thisAmount ersetzt durch 2 Returner-Methoden

--- src/main/java/dhbw/fowler2/theatre/StatementPrinter.java
+++ src/main/java/dhbw/fowler2/theatre/StatementPrinter.java
@@ -6,7 +6,11 @@ import java.util.Map;
 
 public class StatementPrinter {
 
+    Map<String, Play> plays;
+
     public String print(Invoice invoice, Map<String, Play> plays) {
+        this.plays = plays;
+
         var totalAmount = 0;
         var volumeCredits = 0;
         var result = String.format("Statement for %s\n", invoice.customer);
@@ -14,22 +18,29 @@ public class StatementPrinter {
         NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
         for (var perf : invoice.performances) {
-            var play = plays.get(perf.playID);
-            var thisAmount = play.amountFor(perf, play);
 
             // add volume credits
             volumeCredits += Math.max(perf.audience - 30, 0);
             // add extra credit for every ten comedy attendees
-            if ("comedy" == play.type) volumeCredits += Math.floor(perf.audience / 5);
+            if ("comedy" == playReturner(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
             // print line for this order
-            result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
-            totalAmount += thisAmount;
+            result += String.format("  %s: %s (%s seats)\n", playReturner(perf).name, frmt.format(amountReturner(perf) / 100), perf.audience);
+            totalAmount += amountReturner(perf);
         }
         result += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
         result += String.format("You earned %s credits\n", volumeCredits);
         return result;
+    }
 
+    private Play playReturner(Performance perf) {
+        Play play = plays.get(perf.playID);
+        return play;
+    }
+
+    private double amountReturner(Performance perf) {
+        double amount = playReturner(perf).amountFor(perf, playReturner(perf));
+        return amount;
     }
 
 }
